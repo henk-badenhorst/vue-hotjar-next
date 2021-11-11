fab803f97c4ea81d1e83f7348a3998193f120709
Enric Bisbe Gil <enricu@gmail.com>
2021-11-11 16:58:06 +0100

    Update webpack.config.js
    Fixes https://github.com/henk-badenhorst/vue-hotjar-next/issues/8

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist/package'),
     filename: 'index.js',
     libraryTarget: 'umd',
+    globalObject: 'this',
     umdNamedDefine: true
   },
   resolve: {
